3b1078e5953e21fff271b51c2f342c279d79be86
zzAdji <alimzzcmr@gmail.com>
2026-04-29 13:00:43 +0100

    Update AppConfig.java

--- src/main/java/com/library/config/AppConfig.java
+++ src/main/java/com/library/config/AppConfig.java
@@ -1,4 +1,10 @@
 package com.library.config;
+import com.library.repository.BookRepository;
+import com.library.repository.LoanRepository;
+import com.library.repository.UserRepository;
+import com.library.repository.impl.JsonBookRepository;
+import com.library.repository.impl.JsonLoanRepository;
+import com.library.repository.impl.JsonUserRepository;
 import com.library.service.AuthService;
 import com.library.service.BookService;
 import com.library.service.LoanService;
@@ -7,26 +13,29 @@ import com.library.service.UserService;
 
 public final class AppConfig {
 
-    private AppConfig() {
-    }
+    private static final UserRepository userRepository = new JsonUserRepository();
+    private static final BookRepository bookRepository = new JsonBookRepository();
+    private static final LoanRepository loanRepository = new JsonLoanRepository();
+    
+    private AppConfig() {}
 
     public static AuthService authService() {
-        return new AuthService();
+        return new AuthService(userRepository);
     }
 
     public static BookService bookService() {
-        return new BookService();
+        return new BookService(bookRepository);
     }
 
     public static UserService userService() {
-        return new UserService();
+        return new UserService(userRepository);
     }
 
     public static LoanService loanService() {
-        return new LoanService();
+        return new LoanService(userRepository, bookRepository, loanRepository);
     }
 
     public static StatisticsService statisticsService() {
-        return new StatisticsService();
+        return new StatisticsService(bookRepository, loanRepository);
     }
 }
